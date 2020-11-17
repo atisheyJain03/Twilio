e277074fabf7b890ffc2e56927cb61dec64a19aa
Atishey jain <atisheyjain1232@gmail.com>
2020-11-17 16:10:22 +0530

    Added home route in route.js file

--- route.js
+++ route.js
@@ -3,6 +3,10 @@ const sendMessage = require('./Controller/sendMessage')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).send("Welcome")
+})
+
 router.post("/whatsApp", sendMessage);
 
 module.exports = router;
